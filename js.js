18dca8ddb762bf10462887ac034a38b03a97222c
Sherly Ferro <she.eli.fer@gmail.com>
2022-10-31 22:47:50 -0500

    empesando DOM

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 
 
 /////////////////////////////////////////////////////////////////////////ultimo ejemplo
-let Edad = parseInt(prompt('Ingrese su edad por favor'));
+/*let Edad = parseInt(prompt('Ingrese su edad por favor'));
 
 if (Edad > 17) {
     alert('Felicidades usted puede acceder a ver los productos');
@@ -98,6 +98,28 @@ agregaCarrito();
 } else {
     alert('Lo sentimos usted es menor de edad no le podemos mostrar las ofertas');
 }
+*/
+let productos = [
+    { nombre: "aretes", precio: 20, stock: 300, imgUrl: "./img/llamitaof.png" },
+    { nombre: "llamitas", precio: 20, stock: 300, imgUrl: "../img/llamita.jpg" },
+    { nombre: "recordatorios", precio: 20, stock: 300, imgUrl: "https://i.pinimg.com/564x/f7/68/8c/f7688cc1500ed123faaefc13ea54d22b.jpg" },
+    { nombre: "polos", precio: 20, stock: 300, imgUrl: "../img/llamita.jpg" },
+    { nombre: "ponchos", precio: 20, stock: 300, imgUrl: "../img/llamita.jpg" },
+
+]
+let contenedorProductos = document.getElementById("contenedorProductos")
+
+for (const producto of productos) {
+    let tarjetaProducto = document.createElement('div')
+    tarjetaProducto.className = 'producto'
+    tarjetaProducto.innerHTML = `
+    <h3>${producto.nombre}</h3>
+    <h4>${producto.precio}</h4>
+    <p>${producto.stock}</p>
+    <img scr =${producto.imgUrl}>
+    `
 
 
 
+    contenedorProductos.append(tarjetaProducto)
+}
