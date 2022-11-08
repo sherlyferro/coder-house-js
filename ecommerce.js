e3e4605aa05288003d6369eca46a16cb4b39b790
Sherly Ferro <she.eli.fer@gmail.com>
2022-11-08 09:34:10 -0500

    reenvio de tarea

---
+++ ecommerce.js
@@ -0,0 +1,96 @@
+let Edad = parseInt(prompt('Ingrese su edad por favor'));
+
+if (Edad > 17) {
+    alert('Felicidades usted puede acceder a ver los productos');
+
+const productosTienda = [
+    { id: 0, nombre: 'carteras', precio: 120, stock: 800 },
+    { id: 1, nombre: 'polos', precio: 50, stock: 500 },
+    { id: 2, nombre: 'aretes', precio: 30, stock: 400 },
+    { id: 3, nombre: 'llaveros', precio: 10, stock: 580 },
+    { id: 4, nombre: 'sombreros', precio: 70, stock: 200 },
+    { id: 5, nombre: 'pantalones', precio: 110, stock: 700 }
+];
+
+let productos = productosTienda.map(producto => {
+    return { id: producto.id, nombre: producto.nombre, precio: producto.precio }
+
+})
+let mensaje = ''
+productos.forEach(producto => {
+    mensaje = mensaje + ` id : ${producto.id}  Nombre : ${producto.nombre}  Precio : ${producto.precio} \n`
+})
+alert('Tenemos los siguientes productos en ofertapara usted \n' + mensaje)
+
+let carrito = [];
+
+
+function agregaCarrito() {
+    let elegirProducto = prompt('Si desea elegir algun producto coloque el numero correspondiente a : 0  ,  1 ,2  ,3  ,4 o 5 \n y  para salir introdusca el número 100 ')
+    while (isNaN(elegirProducto)) {
+        alert('Porfavor ingrese solo números')
+        
+        elegirProducto = prompt('Si desea elegir algun producto coloque el numero correspondiente 0  ,  1 ,2  ,3  ,4 o 5 \n  y para salir introdusca el número 100 ')
+    }
+    
+    while (elegirProducto != 100) {
+        switch (parseInt(elegirProducto)) {
+            case 0:
+                carrito.push(productosTienda[0])
+                alert(`Agregamos a tu carrito el producto ${productosTienda[0].nombre}`)
+               
+                break;
+            case 1:
+                carrito.push(productosTienda[1])
+                alert(`Agregamos a tu carrito el producto ${productosTienda[1].nombre}`)
+               
+                break;
+            case 2:
+                carrito.push(productosTienda[2])
+                alert(`Agregamos a tu carrito el producto ${productosTienda[2].nombre}`)
+               
+                break;
+            case 3:
+                carrito.push(productosTienda[3])
+                alert(`Agregamos a tu carrito el producto ${productosTienda[3].nombre}`)
+               
+                break;
+            case 4:
+                carrito.push(productosTienda[4])
+                alert(`Agregamos a tu carrito el producto ${productosTienda[4].nombre}`)
+                
+                break;
+            case 5:
+                carrito.push(productosTienda[5])
+                alert(`Agregamos a tu carrito el producto ${productosTienda[5].nombre}`)
+               
+                break;
+            default:
+                alert('No tenemos ese producto')
+                break
+        }
+        elegirProducto = prompt('Si desea elegir algun producto coloque el numero correspondiente 0  ,  1 ,2  ,3  ,4 o 5 \n para salir introdusca el número 100 ')
+    }
+    
+    alert('¡carrito cerrado!')
+    mostrarCarrito()
+}
+
+let productosCarrito = "vas a llevar :\ n"
+let precioCarrito = 0
+
+
+function mostrarCarrito() {
+    for (itemCarrito of carrito) {
+        productosCarrito += `\n - ${itemCarrito.nombre}`
+        precioCarrito += itemCarrito.precio
+    }
+    alert(`Repasemos : \n ${productosCarrito} \n por un total de $: ${precioCarrito}`)
+}
+
+ 
+agregaCarrito();
+
+} else {
+    alert('Lo sentimos usted es menor de edad no le podemos mostrar las ofertas');
+}
--- js.js
+++ js.js
@@ -1,104 +1,3 @@
-
-
-
-/////////////////////////////////////////////////////////////////////////ultimo ejemplo
-/*let Edad = parseInt(prompt('Ingrese su edad por favor'));
-
-if (Edad > 17) {
-    alert('Felicidades usted puede acceder a ver los productos');
-
-const productosTienda = [
-    { id: 0, nombre: 'carteras', precio: 120, stock: 800 },
-    { id: 1, nombre: 'polos', precio: 50, stock: 500 },
-    { id: 2, nombre: 'aretes', precio: 30, stock: 400 },
-    { id: 3, nombre: 'llaveros', precio: 10, stock: 580 },
-    { id: 4, nombre: 'sombreros', precio: 70, stock: 200 },
-    { id: 5, nombre: 'pantalones', precio: 110, stock: 700 }
-];
-
-let productos = productosTienda.map(producto => {
-    return { id: producto.id, nombre: producto.nombre, precio: producto.precio }
-
-})
-let mensaje = ''
-productos.forEach(producto => {
-    mensaje = mensaje + ` id : ${producto.id}  Nombre : ${producto.nombre}  Precio : ${producto.precio} \n`
-})
-alert('Tenemos los siguientes productos en ofertapara usted \n' + mensaje)
-
-let carrito = [];
-
-
-function agregaCarrito() {
-    let elegirProducto = prompt('Si desea elegir algun producto coloque el numero correspondiente a : 0  ,  1 ,2  ,3  ,4 o 5 \n y  para salir introdusca el número 100 ')
-    while (isNaN(elegirProducto)) {
-        alert('Porfavor ingrese solo números')
-        
-        elegirProducto = prompt('Si desea elegir algun producto coloque el numero correspondiente 0  ,  1 ,2  ,3  ,4 o 5 \n  y para salir introdusca el número 100 ')
-    }
-    
-    while (elegirProducto != 100) {
-        switch (parseInt(elegirProducto)) {
-            case 0:
-                carrito.push(productosTienda[0])
-                alert(`Agregamos a tu carrito el producto ${productosTienda[0].nombre}`)
-               
-                break;
-            case 1:
-                carrito.push(productosTienda[1])
-                alert(`Agregamos a tu carrito el producto ${productosTienda[1].nombre}`)
-               
-                break;
-            case 2:
-                carrito.push(productosTienda[2])
-                alert(`Agregamos a tu carrito el producto ${productosTienda[2].nombre}`)
-               
-                break;
-            case 3:
-                carrito.push(productosTienda[3])
-                alert(`Agregamos a tu carrito el producto ${productosTienda[3].nombre}`)
-               
-                break;
-            case 4:
-                carrito.push(productosTienda[4])
-                alert(`Agregamos a tu carrito el producto ${productosTienda[4].nombre}`)
-                
-                break;
-            case 5:
-                carrito.push(productosTienda[5])
-                alert(`Agregamos a tu carrito el producto ${productosTienda[5].nombre}`)
-               
-                break;
-            default:
-                alert('No tenemos ese producto')
-                break
-        }
-        elegirProducto = prompt('Si desea elegir algun producto coloque el numero correspondiente 0  ,  1 ,2  ,3  ,4 o 5 \n para salir introdusca el número 100 ')
-    }
-    
-    alert('¡carrito cerrado!')
-    mostrarCarrito()
-}
-
-let productosCarrito = "vas a llevar :\ n"
-let precioCarrito = 0
-
-
-function mostrarCarrito() {
-    for (itemCarrito of carrito) {
-        productosCarrito += `\n - ${itemCarrito.nombre}`
-        precioCarrito += itemCarrito.precio
-    }
-    alert(`Repasemos : \n ${productosCarrito} \n por un total de $: ${precioCarrito}`)
-}
-
- 
-agregaCarrito();
-
-} else {
-    alert('Lo sentimos usted es menor de edad no le podemos mostrar las ofertas');
-}
-*/
 let productos = [
     { nombre: "aretes", precio: 20, stock: 300, imgUrl: "./img/llamitaof.png" },
     { nombre: "llamitas", precio: 20, stock: 300, imgUrl: "../img/llamita.jpg" },
@@ -123,3 +22,23 @@ for (const producto of productos) {
 
     contenedorProductos.append(tarjetaProducto)
 }
+/*
+#contenedorProductos {
+    width: 800px;
+    height: 800px;
+    background-color: aquamarine;
+    display: flex;
+    color: black;
+}
+
+.producto {
+    width: 700px;
+    height: 500px;
+    background-color: rgb(251, 139, 242);
+    margin: 5px;
+   border: 1px solid black;
+}
+img{
+    width: 80px;
+    height: 80px;
+}*/
